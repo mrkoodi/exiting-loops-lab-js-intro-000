d487fca43f7ba8f3477aadd00d4be9c60e7dba9b
Ebenezer Koodi <mrrkoodi@gmail.com>
2018-01-26 15:58:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
       for (var i = 0; i < array.length; i++) {
-        if ( array[i] !== findFn)
-        return null;
+        if ( findFn(array[i]))
+        return array[i];
       }
-      return array[i];
 }
